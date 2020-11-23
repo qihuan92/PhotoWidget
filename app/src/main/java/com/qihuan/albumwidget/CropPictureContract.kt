e4070ee1074599153d6f747af2b3e9df1aa5596a
qihuan <qihuan92@126.com>
2020-11-23 11:27:17 +0800

    UCrop参数调整

--- app/src/main/java/com/qihuan/albumwidget/CropPictureContract.kt
+++ app/src/main/java/com/qihuan/albumwidget/CropPictureContract.kt
@@ -4,6 +4,7 @@ import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.graphics.Bitmap
+import android.graphics.Color
 import android.net.Uri
 import androidx.activity.result.contract.ActivityResultContract
 import com.yalantis.ucrop.UCrop
@@ -18,6 +19,10 @@ class CropPictureContract : ActivityResultContract<CropPictureInfo, Uri?>() {
     override fun createIntent(context: Context, input: CropPictureInfo): Intent {
         return UCrop.of(input.inUri, input.outUri)
             .withOptions(UCrop.Options().apply {
+                setStatusBarColor(context.getColor(R.color.purple_700))
+                setToolbarWidgetColor(Color.WHITE)
+                setToolbarColor(context.getColor(R.color.purple_500))
+                setActiveControlsWidgetColor(context.getColor(R.color.purple_500))
                 setCompressionFormat(Bitmap.CompressFormat.PNG)
             })
             .withMaxResultSize(1000, 1000)
