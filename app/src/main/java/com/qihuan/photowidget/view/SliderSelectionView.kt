e4b9242e52e479b219401e22170b0974579c21e3
qihuan <qihuan92@126.com>
2021-10-21 11:11:32 +0800

    配置页面选项UI调整

--- app/src/main/java/com/qihuan/photowidget/view/SliderSelectionView.kt
+++ app/src/main/java/com/qihuan/photowidget/view/SliderSelectionView.kt
@@ -1,19 +1,22 @@
 package com.qihuan.photowidget.view
 
+import android.annotation.SuppressLint
 import android.content.Context
-import android.graphics.Typeface
+import android.graphics.drawable.Drawable
 import android.util.AttributeSet
-import android.view.Gravity
+import android.view.LayoutInflater
 import android.widget.LinearLayout
-import android.widget.TextView
+import androidx.core.view.isVisible
 import com.google.android.material.slider.Slider
 import com.qihuan.photowidget.R
+import com.qihuan.photowidget.databinding.LayoutSliderSelectionBinding
 
 /**
  * SliderSelectionView
  * @author qi
  * @since 2021/10/8
  */
+@SuppressLint("SetTextI18n")
 class SliderSelectionView : LinearLayout {
 
     private var title: String? = null
@@ -21,22 +24,11 @@ class SliderSelectionView : LinearLayout {
     private var valueFrom: Float = 0f
     private var valueTo: Float = 20f
     private var stepSize: Float = 1f
+    private var icon: Drawable? = null
+    private var unit: String = ""
 
-    private val titleView by lazy {
-        TextView(context).apply {
-            setTextAppearance(android.R.style.TextAppearance_Material_Body2)
-            typeface = Typeface.DEFAULT_BOLD
-            text = title
-        }
-    }
-
-    private val contentView by lazy {
-        Slider(context).apply {
-            value = this@SliderSelectionView.value
-            valueFrom = this@SliderSelectionView.valueFrom
-            valueTo = this@SliderSelectionView.valueTo
-            stepSize = this@SliderSelectionView.stepSize
-        }
+    private val binding by lazy(LazyThreadSafetyMode.NONE) {
+        LayoutSliderSelectionBinding.inflate(LayoutInflater.from(context), this, true)
     }
 
     constructor(context: Context) : this(context, null)
@@ -52,32 +44,41 @@ class SliderSelectionView : LinearLayout {
             valueFrom = getFloat(R.styleable.SliderSelectionView_sliderSelectionValueFrom, 0f)
             valueTo = getFloat(R.styleable.SliderSelectionView_sliderSelectionValueTo, 20f)
             stepSize = getFloat(R.styleable.SliderSelectionView_sliderSelectionStepSize, 1f)
+            icon = getDrawable(R.styleable.SliderSelectionView_sliderSelectionIcon)
+            unit = getString(R.styleable.SliderSelectionView_sliderSelectionValueUnit) ?: ""
         }.recycle()
         initView()
     }
 
     private fun initView() {
-        orientation = HORIZONTAL
-        gravity = Gravity.CENTER_VERTICAL
-        addView(titleView, LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT)
-        addView(contentView, LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT)
+        binding.tvTitle.text = title
+        binding.slider.apply {
+            value = this@SliderSelectionView.value
+            valueFrom = this@SliderSelectionView.valueFrom
+            valueTo = this@SliderSelectionView.valueTo
+            stepSize = this@SliderSelectionView.stepSize
+        }
+        binding.ivIcon.isVisible = icon != null
+        binding.ivIcon.setImageDrawable(icon)
+        binding.tvValue.text = "${value}${unit}"
     }
 
     fun setTitle(value: String) {
         this.title = value
-        titleView.text = value
+        binding.tvTitle.text = value
     }
 
-    fun getTitle(): CharSequence = titleView.text
+    fun getTitle(): CharSequence = binding.tvTitle.text
 
     fun setValue(value: Float) {
         this.value = value
-        contentView.value = value
+        binding.slider.value = value
+        binding.tvValue.text = "${value}${unit}"
     }
 
-    fun getValue() = contentView.value
+    fun getValue() = binding.slider.value
 
     fun addOnChangeListener(listener: Slider.OnChangeListener) {
-        contentView.addOnChangeListener(listener)
+        binding.slider.addOnChangeListener(listener)
     }
 }
