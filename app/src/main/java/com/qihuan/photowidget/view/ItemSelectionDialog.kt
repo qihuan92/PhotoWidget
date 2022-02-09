967207ceee63e3b92311390ee50f72f669b27346
qihuan <qihuan92@126.com>
2022-02-09 15:39:27 +0800

    选择弹框调整

--- app/src/main/java/com/qihuan/photowidget/view/ItemSelectionDialog.kt
+++ app/src/main/java/com/qihuan/photowidget/view/ItemSelectionDialog.kt
@@ -3,13 +3,16 @@ package com.qihuan.photowidget.view
 import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.DrawableRes
 import androidx.core.view.isVisible
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.qihuan.photowidget.R
 import com.qihuan.photowidget.databinding.DialogItemSelectionBinding
 import com.qihuan.photowidget.databinding.ItemDialogSelectionBinding
 import com.qihuan.photowidget.ktx.performHapticFeedback
@@ -48,6 +51,15 @@ class ItemSelectionDialog<T : ItemSelectionDialog.Item>(
 
     fun setItemList(itemList: List<T>) {
         adapter.submitList(itemList)
+    }
+
+    override fun show() {
+        super.show()
+        val view = findViewById<View>(R.id.design_bottom_sheet)
+        view?.post {
+            val behavior = BottomSheetBehavior.from(view)
+            behavior.setState(BottomSheetBehavior.STATE_EXPANDED)
+        }
     }
 
     class Adapter<T : Item>(
