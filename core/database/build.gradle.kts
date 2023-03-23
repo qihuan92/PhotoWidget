24d6da4868f3cf5900ac1b20e7142a58e2436896
qihuan <qihuan92@126.com>
2023-03-23 15:30:21 +0800

    build:更新依赖版本

--- core/database/build.gradle.kts
+++ core/database/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
     id("kotlin-parcelize")
-    id("com.google.devtools.ksp").version("1.7.10-1.0.6")
+    id("com.google.devtools.ksp").version("1.8.10-1.0.9")
 }
 
 android {
